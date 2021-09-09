0b007e2df763ae3a77bf2bea790120a101ba6653
insertbaek <insert.baek@gmail.com>
2021-09-09 15:54:57 +0900

    shell to vscode

---
+++ app/nodejs/app.js
@@ -0,0 +1,58 @@
+const app = require('express')();
+const http = require('http').createServer(app);
+
+app.get('/', (req, res) => {
+	res.sendFile('/home/dev02.01/public/index.html');
+});
+
+http.listen(3000, () => {
+	console.log('Connected at 3000')
+});
+
+/*
+
+//const path = require("path"); 
+const express = require('express'); 
+const app = express(); 
+
+//console.log(__dirname);
+
+app.use("/", (req, res)=>{ 
+	res.sendFile('/home/dev02.01/public/index.html'); // index.html 파일 응답 
+}) 
+
+const HTTPServer = app.listen(30001, ()=>{ 
+	console.log("Server is open at port:30001"); 
+}); 
+
+const wsModule = require('ws'); 
+
+const webSocketServer = new wsModule.Server( 
+	{ 
+		server: HTTPServer,
+	} 
+); 
+
+webSocketServer.on('connection', (ws, request)=>{ 
+	const ip = request.headers['x-forwarded-for'] || request.connection.remoteAddress; 
+	console.log(`새로운 클라이언트[${ip}] 접속`); 
+	
+	if(ws.readyState === ws.OPEN){ 
+		ws.send(`클라이언트[${ip}] 접속을 환영합니다 from 서버`); // 데이터 전송 
+	} 
+	
+	ws.on('message', (msg)=>{ 
+		console.log(`클라이언트[${ip}]에게 수신한 메시지 : ${msg}`); 
+		ws.send('메시지 잘 받았습니다! from 서버') 
+	}) 
+	
+	ws.on('error', (error)=>{ 
+		console.log(`클라이언트[${ip}] 연결 에러발생 : ${error}`); 
+	}) 
+	
+	ws.on('close', ()=>{ 
+		console.log(`클라이언트[${ip}] 웹소켓 연결 종료`); 
+	}) 
+});
+
+*/
