1b0c55cea56d41675016110c130f3ba6dde5e335
insertbaek <insert.baek@gmail.com>
2021-09-15 14:13:26 +0900

    2021-09-15 python config

--- app/python/dev_work/ib_off_work.py
+++ app/python/dev_work/ib_off_work.py
@@ -16,43 +16,46 @@ CFilePath = cfg.CFilepathInfo()
 CDev02dbMaster = cfg.CDev02dbMaster()
 
 try:
-    for CConfig in [CFilePath, CDev02dbMaster]:
-        bResult = Cvalidate.isDictEmpty(CConfig.__dict__)
+	for CConfig in [CFilePath, CDev02dbMaster]:
+		bResult = Cvalidate.isDictEmpty(CConfig.__dict__)
 
-        if not bResult:
-            raise Exception(Cvalidate.getDictEmpty())
+		if not bResult:
+			raise Exception(Cvalidate.getDictEmpty())
 except Exception as e:
-    print('환경설정 중' + str(e) + '항목 정의가 올바르지 않습니다.')
-    sys.exit()
+	print('환경설정 중' + str(e) + '항목 정의가 올바르지 않습니다.')
+	sys.exit()
 finally:
-    del CConfig, bResult
+	del CConfig, bResult
 
 # 참고
 # 로그관련 정의
 strSysLogFileName = "".join([strProcessRunTime, '_', CFilePath.alias, '.log'])
 CibLogSys = fn.CibLog(CFilePath.backup_syslog, str(strSysLogFileName), 'backup')
-CDev02MasterDbconn = pymysql.connect(host=CDev02dbMaster.host, user=CDev02dbMaster.user, password=CDev02dbMaster.password, port=CDev02dbMaster.port, charset='utf8')
+CDev02MasterDbconn = pymysql.connect(host=CDev02dbMaster.host, user=CDev02dbMaster.user, password=CDev02dbMaster.password, db=CDev02dbMaster.db, port=CDev02dbMaster.port, charset='utf8')
+
+print(CDev02MasterDbconn)
 
 try:
-    with CDev02MasterDbconn.cursor(pymysql.cursors.DictCursor) as CDev02MasterDbconnCurs:
-        qryTableInfo = "SHOW TABLES;"
-        CDev02MasterDbconnCurs.execute(qryTableInfo)
-        rstTableInfoList = CDev02MasterDbconnCurs.fetchone()
+	with CDev02MasterDbconn.cursor(pymysql.cursors.DictCursor) as CDev02MasterDbconnCurs:
+		qryTableInfo = "SHOW TABLES;"
+		CDev02MasterDbconnCurs.execute(qryTableInfo)
+		rstTableInfoList = CDev02MasterDbconnCurs.fetchall()
 
-        print(rstTableInfoList)
+		print(rstTableInfoList)
 
+        #print(rstTableInfoList)
         #if rstTableInfoList['nBackupTableCount'] < 1:
             #raise Exception('백업 대상 테이블이 존재하지 않습니다.')
 except Exception as e:
-    if Cvalidate.isEmpty(e) == False:
-        CibLogSys.error('심각한 오류가 발생하였습니다.')
-    else:
-        CibLogSys.error(e)
-    sys.exit()
+    #if Cvalidate.isEmpty(e) == False:
+        #CibLogSys.error('심각한 오류가 발생하였습니다.')
+    #else:
+        #CibLogSys.error(e)
+	sys.exit()
 finally:
-    CibLogSys.info(qryTableInfo + ' [result : ' + str(qryTableInfo)  + ']')
-    CibLogSys.debug(qryTableInfo)
-    del CDev02MasterDbconnCurs, qryTableInfo
+    #CibLogSys.info(qryTableInfo + ' [result : ' + str(qryTableInfo)  + ']')
+    #CibLogSys.debug(qryTableInfo)
+	del CDev02MasterDbconnCurs, qryTableInfo
 
 
 print(CFilePath.python)
