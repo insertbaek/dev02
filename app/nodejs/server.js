9cbeaff0b1bd22843ae9368f6fd25c445e05694c
insertbaek <insert.baek@gmail.com>
2021-09-16 13:15:52 +0900

    2021-09-16 nodejs init

--- app/nodejs/server.js
+++ app/nodejs/server.js
@@ -25,7 +25,7 @@ app.get('/', (req, res) => {
 });
 
 http.listen(3000, () => {
-	console.log('Connected at 3000')
+	console.log('Connected at 3000');
 });
 
 io.on('connection', (socket)=>{
@@ -38,3 +38,40 @@ io.on('connection', (socket)=>{
     });
 });
 
+var os = require('os');
+
+  
+
+function getServerIp() {
+
+    var ifaces = os.networkInterfaces();
+
+    var result = '';
+
+    for (var dev in ifaces) {
+
+        var alias = 0;
+
+        ifaces[dev].forEach(function(details) {
+
+            if (details.family == 'IPv4' && details.internal === false) {
+
+                result = details.address;
+
+                ++alias;
+
+            }
+
+        });
+
+    }
+
+  
+
+    return result;
+
+}
+
+  
+
+console.log(getServerIp());
