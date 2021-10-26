19306b86cb333320f7229923a5cbbaed833e409d
cynical1031 <cynicalkim@gmail.com>
2021-10-26 17:54:14 +0900

    211026 api 폴더구조 설정

--- app/nodejs/src/controller/user.controller.js
+++ app/nodejs/src/controller/user.controller.js
@@ -1,19 +1,25 @@
 const ApiError = require('../utils/ApiError');
 const catchAsync = require('../utils/catchAsync');
 const createUser = catchAsync(async (req, res) => {
+    //req.body -> post 등 방식 데이터 가져오기
+    console.log(req.body)
     res.status(201).send(user);
 });
 
 const getUsers = catchAsync(async (req, res) => {
+    //req.query -> get 방식 데이터 가져오기
+    console.log(req.query)
     const result = {}
     res.send(result);
 });
 
 const getUser = catchAsync(async (req, res) => {
-    const user = false;
-    if (!user) {
-        throw new ApiError(401, 'User not found');
-    }
+    //req.params.parm -> url param 데이터 가져오기
+    const user = {};
+    // if (!user) {
+    //     throw new ApiError(401, 'User not found');
+    // }
+    console.log(req.params.userId)
     res.send(user);
 });
 
