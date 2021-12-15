bc945f2567dc33e8cc3d8851e616be467903e89d
ohjinsol <ohjinsol77@gmail.com>
2021-12-15 19:03:17 +0900

    2021-12-15 미로생성 신규 컨트롤러 작성

--- app/python/controller/maze/MazePlayController.py
+++ app/python/controller/maze/MazePlayController.py
@@ -5,7 +5,9 @@
  @update
  @description
 '''
-import sys, os, datetime, json, math, random, numpy
+import sys, os, datetime, json, math
+from random import randint
+import numpy as np
 sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))))
 from config import ib_config as cfg
 from config import ib_function as fn
@@ -146,149 +148,97 @@ class MazePlayControll:
 
 
 class MazeMakeControll:
-    def __init__(self, width = 5, height = 5):
-        print("###################")
-        self.cells = []
-        self.width=width
-        self.height=height
 
-        self.generateMaze(width, height)
-        # self.cells = self.generateMaze(width, height)
-        self.solution = []
-        self.solution2 = []
-        
-        
-        self.visited = False
+    # def __init__(self, nStartX, nStartY, nEndX, nEndY, nMazeX, nMazeY, nLevel):
 
+    #     print(nStartX, nStartY, nEndX, nEndY, nMazeX, nMazeY, nLevel)
 
-    def generateMaze(self, width, height):
-        generateCells = [range(width)]
-        for x in range(0, width-1, 1):
-            generateCells.append([range(height)])
-            for y in range(0, height-1, 1):
-                generateCells[x][y] = Cell(x, y)
-        startX = math.floor(random.randrange(0, width-1))
-        startY =  math.floor(random.randrange(0,height-1))
-        print("THIS",startX,width,startY,height)
-        self.cells = generateCells
+    #     self.rgStart = [nStartX,nStartY]
+    #     self.rgEnd = [nEndX,nEndY]
+    #     self.nMazeX = nMazeX
+    #     self.nMazeY = nMazeY
+    #     self.nLevel = nLevel
 
-        self.visit(generateCells[startX][startY])
-        solution = self.solve(width - 1, height - 1, 0, 0)
-        solution2 = self.solve(0, height - 1, width-1, 0)
-        if solution.length == solution2.length:
-            self.solution = solution
-            self.solution2 = solution2
-            self.cells = generateCells
-            # return generateCells
-        else:
-            self.generateMaze(width, height)
+    #     rstCreat_map = self.creat_map(self, self.rgStart, self.rgEnd, self.nMazeX, self.nMazeY, self.nLevel)
 
-    def getOptions(self, cell, walls):
-        options = []
-        
-        def checkOption(x, y):
-            print("################!#@#",x,y)
-            if (walls):
-                if ((y < cell.y and cell.walls['n']) or (x > cell.x and cell.walls['e']) or (y > cell.y and cell.walls['s']) or (x < cell.x and cell.walls['w'])):
-                    print("x5-1")
-                    return
-            print("5-2")
-            if(self.cells[x] is not None):
-                print("@1//")
-                if(self.cells[x][y] is not None):
-                    print("@2//",x,y)
-                    if(not self.cells[x][y].visited):
-                        print("@3//")
-                        options.append(self.cells[x][y])
+    #     print(rstCreat_map)
 
-            print("@4//")
-            #if (self.cells[x] is not None and self.cells[x][y] is not None and not self.cells[x][y].visited):
-                #options.append(self.cells[x][y])
-        
-        checkOption(cell.x, cell.y - 1)
-        checkOption(cell.x + 1, cell.y)
-        checkOption(cell.x, cell.y + 1)
-        checkOption(cell.x - 1, cell.y)
+    def __init__(self):
+        pass
 
-        return options
+    def fnCreateMap(self, rgGameInfo):
 
-    def visit(self, cell, direction = None):
-        cell.visited = True
-        if (direction is not None) :
-            cell.walls[direction] = False
-        
-        cell.visited = True
+        # 지정된 순서 규약
+        start_node=[rgGameInfo[0],rgGameInfo[1]]
+        end_node=[rgGameInfo[2],rgGameInfo[3]]
+        mazeX=rgGameInfo[4]
+        mazeY=rgGameInfo[5]
+        sparsity=rgGameInfo[6]
 
-        print("xVisit")
-        options = self.getOptions(cell, False)
-        print("xVisit1")
-        print(options)
+        nSizeX = mazeX
+        nSizeY = mazeY
+        maxWall = 13
+        rgMaze = (nSizeX*nSizeY)*[0]
 
-        while (len(options) > 0):
-            print("xVisit2")
+        for i in range(0,nSizeX,sparsity):
+            for j in range(0,nSizeY,sparsity):
 
-            index =  math.floor(random.randrange(0,1)*len(options))
-            nextCell = options[index]
-            print("xVisit3")
-            print(nextCell.x, nextCell.y)
-            if cell.y > nextCell.y:
-                cell.walls['n'] = False
-                self.visit(nextCell, "s")
-            elif cell.x < nextCell.x:
-                cell.walls['e'] = False
-                self.visit(nextCell, "w")
-            elif cell.y < nextCell.y:
-                cell.walls['s'] = False
-                self.visit(nextCell, "n")
-            elif cell. x > nextCell.x:
-                cell.walls['w'] = False
-                self.visit(nextCell, "e")
+                rgMaze[(j*nSizeX)+i]=2
 
-            options = self.getOptions(cell)
+        for i in range(0,nSizeX):
+            rgMaze[i]=1
+            rgMaze[(nSizeX*(nSizeY-1))+i] = 1
+        for i in range(1,nSizeY-1):
+            rgMaze[nSizeX*i]=1
+            rgMaze[(nSizeX*i)+(nSizeX-1)] = 1
 
-    def solve(self, startX, startY, endX, endY):
-        startX = startX or 0
-        startY = startY or 0
-        endX = endX == None and self.width - 1 or endX
-        endY = endY == None and self.height - 1 or endY
-        print("x6")
-        print("!!!!!!!!!",self.cells)
-        for x in range(0, len(self.cells), 1):
-            for y in range(0, len(self.cells[x]), 1):
-                self.cells[x][y].visited = False
-        print("x7",self.cells[0][0].walls['n'])
-        solution = []
-        cell = self.cells[startX][startY]
-        options = []
-        print("x8")
-        while ((cell.x != endX) or (cell.y != endY)):
-            cell.visited = True
-            options = self.getOptions(cell, True)
-            print("option",options)
-            if (len(options) == 0):
-                print(solution)
-                cell = solution.pop()
-            else:
-                solution.push(cell)
-                cell = options[0]
-        print("x9")
-        solution.push(cell)
+        while 2 in rgMaze:
+            x = randint(1,(nSizeX-1)/2)*2
+            y = randint(1,(nSizeY-1)/2)*2
+            c = rgMaze[(y*nSizeX)+x]
 
-        return solution
+            if c == 2:
+                mwc = 0
+                r = randint(0,3)
+                if r == 0:
+                    while c != 1:
+                        rgMaze[(y*nSizeX)+x] = 1
+                        y -= 1
+                        c = rgMaze[(y*nSizeX)+x]
+                        mwc += 1
+                        if mwc == maxWall: break
+                if r == 1:
+                    while c != 1:
+                        rgMaze[(y*nSizeX)+x] = 1
+                        x += 1
+                        c = rgMaze[(y*nSizeX)+x]
+                        mwc += 1
+                        if mwc == maxWall: break
+                if r == 2:
+                    while c != 1:
+                        rgMaze[(y*nSizeX)+x] = 1
+                        y += 1
+                        c = rgMaze[(y*nSizeX)+x]
+                        mwc += 1
+                        if mwc == maxWall: break
+                if r == 3:
+                    while c!= 1:
+                        rgMaze[(y*nSizeX)+x] = 1
+                        x -= 1
+                        c = rgMaze[(y*nSizeX)+x]
+                        mwc += 1
+                        if mwc == maxWall: break
 
-    def getData(self):
-        print(self.cells)
-   
-class Cell:
-   def __init__(self, x, y):
-       self.x: int = x
-       self.y: int = y
+        rgMazeMap={}
+        rgArrMake=np.ones((nSizeX,nSizeY))
 
-       self.walls = {
-           'n' : True,
-           'e' : True,
-           's' : True,
-           'w' : True
-       }
-
-       self.visited = False
+        for i in range(0,nSizeY): 
+            for j in range(0,nSizeX):
+                k = rgMaze[(i*nSizeX)+j]
+                if k == 1:
+                    if (i==start_node[0]  and j==start_node[1]) or (j==end_node[1]and i==end_node[0]):
+                        continue
+                    else:
+                        rgMazeMap[(i,j)]=1
+                        rgArrMake[j][i]=0
+        return rgMazeMap
